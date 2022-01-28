54e362aedbc618918663c75cfaa0b83484f959b8
Theodoro Cardoso <cardoso.theodoro@gmail.com>
2022-01-28 17:50:39 +0000

    Up time function added and tested at main.cpp

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -88,7 +88,16 @@ float LinuxParser::MemoryUtilization() {
 }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  string line, value;
+  std::ifstream filestream(kProcDirectory + kUptimeFilename);
+  if (filestream.is_open()) {
+      std::getline(filestream, line);
+      std::istringstream linestream(line);
+      linestream >> value;
+      return stol(value);
+  }
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "ncurses_display.h"
 #include "system.h"
-
+#include <iostream>
+#include "linux_parser.h"
 int main() {
   //Testing clone
+  std::cout << LinuxParser::UpTime() << std::endl;
   System system;
-  NCursesDisplay::Display(system);
+  //NCursesDisplay::Display(system);
 }
